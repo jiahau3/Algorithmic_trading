71b64a14724545753c4fda4000cf8a7bc45ab713
jiahau3 <gn01735570@gmail.com>
2022-06-22 17:16:01 +0800

    Added trendfollowing into automation

--- automation.sh
+++ automation.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 function GetLiveData { python GetLiveData.py; }
-
+function Trendfollowing { python trendfollowing.py; }
 
 while true; do case $1 in
 	GetLiveData) GetLiveData;	shift	;;
+	Trendfollowing) Trendfollowing;	shift	;;
 	*) break;;
   esac
 done
